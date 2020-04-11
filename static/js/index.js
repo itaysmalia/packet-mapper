d08f8591ae5d2c5d54e020f74a6852fcfa3054b8
itaysmalia <itays.malia@gmail.com>
2020-04-12 01:18:29 +0300

    fixed first run timing problem

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,7 @@
 let i = 0;
 const conteiner = $('.conteiner')
 let mydata = {}
-$.get('/mydata',(data) => {
-    console.log('my data - ',data)
-    mydata = data;
-    document.documentElement.style.setProperty('--mylo',data.longitude)
-    document.documentElement.style.setProperty('--myla',data.latitude)
-    $('body').find('div#loading').remove()
-    conteiner.append(`<div class=\"me\"></div>`)
-})
+
 const getpkt = () => {
     $.get('/pkt', (data) => {
         if (data.country){
@@ -22,4 +15,16 @@ const getpkt = () => {
     })
     setTimeout(getpkt, 50);
 }
-setTimeout(getpkt, 50);
+const getMyData = () => {
+    $.get('/mydata',(data) => {
+        console.log('my data - ',data)
+        mydata = data;
+        document.documentElement.style.setProperty('--mylo',data.longitude)
+        document.documentElement.style.setProperty('--myla',data.latitude)
+        $('body').find('div#loading').remove()
+        conteiner.append(`<div class=\"me\"></div>`)
+        setTimeout(getpkt, 50);
+    })
+}
+setTimeout(getMyData,3000)
+
